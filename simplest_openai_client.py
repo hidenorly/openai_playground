6f6db07ae2d0b49a00abb3aed4e2c0c4cadf6f40
hidenorly <twitte.harold@gmail.com>
2024-09-22 10:47:04 +0900

    Add simplest_openai_client.py

---
+++ simplest_openai_client.py
@@ -0,0 +1,34 @@
+#   Copyright 2024 hidenorly
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import os
+from openai import AzureOpenAI
+
+client = AzureOpenAI(
+  api_key = os.getenv("AZURE_OPENAI_API_KEY"),  
+  api_version = "2024-02-01",
+  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+)
+
+response = client.chat.completions.create(
+    model= os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"), #"gpt-35-turbo-instruct", # model = "deployment_name".
+    messages=[
+        {"role": "system", "content": "Assistant is a large language model trained by OpenAI."},
+        {"role": "user", "content": "Who were the founders of Microsoft?"}
+    ]
+)
+
+print(response)
+#print(response.model_dump_json(indent=2))
+#print(response.choices[0].message.content)
